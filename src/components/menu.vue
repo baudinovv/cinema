1efbaef4aa190b89ebcc3b15db19214008284d5c
Alisher <alisher.baudin@mail.ru>
2024-09-07 19:43:12 +0500

    test

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
   <aside class="bg-black w-.5/10 h-lvh">
-    <span class="icon-[ri--home-line]"></span>
+    <span class="icon-[ri--home-line]">a</span>
   </aside>
 </template>
 
